fa950f3d42e708cb7ca5e93bb84dae4d4c4c0102
omarashrafayad <omar.ayad3040@gmail.com>
2026-07-07 11:23:29 +0300

    feat: add SpecialOfferType interface to define structure for special offers

--- types/specialOffer.ts
+++ types/specialOffer.ts
@@ -1,10 +1,7 @@
 export interface SpecialOfferType {
     id: number;
     imageName: string;
-<<<<<<< HEAD
     // imagePath: string;
-=======
->>>>>>> feat/login_and_register
     sectionNum: number;
     link?: string | null;
 }
